615df7d081716639c9e185c305c45e40c2bcb47c
Rishabh2482 <rishabh2002chauhan@gmail.com>
2025-07-16 00:36:34 +0530

    Created backend api and database part

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,3 +8,43 @@ export const getUsers = async (req, res) =>{
         res.status(500).json({ message: error.message });
     }
 }
+
+export const getUserById = async(req, res)=>{
+    try{
+        const user = await MERNUser.findById(req.params.id);
+        if(!user){
+            return res.status(400).json({message: 'User not found'});
+        }
+        res.json(user);
+    }catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const saveUser = async(req, res)=>{
+    const user = new MERNUser(req.body);
+    try{
+        const savedUser = await user.save();
+        res.status(201).json(savedUser); // 201 status code for created resource
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+}
+
+export const updateUser = async(req, res) =>{
+    try{
+        const updateduser = await MERNUser.updateOne({_id:req.params.id}, {$set: req.body});
+        res.status(200).json(updateduser);
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+}
+
+export const deleteUser = async(req, res)=>{
+    try{
+        const deletedUser = await MERNUser.deleteOne({_id:req.params.id});
+        res.json(200).json(deletedUser);
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+}
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { getUsers } from '../controllers/UserController.js';
+import { deleteUser, getUserById, getUsers, saveUser, updateUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.get('/users', getUsers); // route to get all users
-// router.get('/users/:id', getUserById);;
-// router.post('/users', saveUser);
-// router.put('/users/:id', updateUser);
-// router.delete('/users/:id', deleteUser);
+router.get('/users/:id', getUserById);;
+router.post('/users', saveUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
+
+// put and patch are similer used to update but if put is used than give all the value, but using patch we only have to give the updated value.
